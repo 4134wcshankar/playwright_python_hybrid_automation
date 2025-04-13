19704bc88d45e94912deb8165808b5d833300239
Sankara <4134.wc.shankar@gmail.com>
2025-04-13 12:33:29 -0500

    modified framework

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# playwright_python_hybrid_automation
-playwright hybrid automation
+playwright_python_hybrid_automation
+
+
+Step 1:
+    - Create a virtual env
+        - python3 -m venv venv_playwright_python_hybrid
+    - Install all requirements for 
+        -  pip3 install -r requirements.txt 
+    - Install playwright
+        - pip install pytest-playwright
+        - playwright install
+    - Install allure
+        - brew install allure
+        - pip install allure-python-commons
+        - pip install allure-pytest
+        
+
+
+To Eexceute Tests On Mac:
+
+Move to Project directory
+
+To Run Tests, Generate reports and view html reports:s
+- Project Dir: 
+    - (venv_hybrid_playwright) bash-3.2$ cd /Users/{YourUserName}/python_test_env/playwright_python_hybrid_pom
+- Run Tests and generate report:
+    - (venv_hybrid_playwright) bash-3.2$ pytest ./test_cases/car_model_test.py --alluredir=./reports/allure_results
+- View Reports:
+    - (venv_hybrid_playwright) bash-3.2$ allure serve ./reports/allure_results
--- pages/BasePage.py
+++ pages/BasePage.py
@@ -1,8 +1,8 @@
-from utilities import configReader
+from utils import config_reader
 import logging
 import allure
 
-from utilities.LogUtil import Logger
+from utils.logger import Logger
 
 log = Logger(__name__,logging.INFO)
 
@@ -13,15 +13,15 @@ class BasePage:
 
     def click(self, locator):
         with allure.step(f"Clicking on an Element {locator}"):
-          self.page.locator(configReader.readConfig("locators",locator)).click()
+          self.page.locator(config_reader.readConfig("locators",locator)).click()
           log.logger.info(f"Clicking on an Element {locator}")
 
     def type(self, locator, value):
         with allure.step(f"Typing in an Element {locator} and entered value as {value}"):
-          self.page.locator(configReader.readConfig("locators",locator)).fill(value)
+          self.page.locator(config_reader.readConfig("locators",locator)).fill(value)
           log.logger.info(f"Typing in an Element {locator} and entered value as {value}")
 
     def move_to(self, locator):
         with allure.step(f"Moving to an Element {locator}"):
-          self.page.locator(configReader.readConfig("locators",locator)).hover()
+          self.page.locator(config_reader.readConfig("locators",locator)).hover()
           log.logger.info(f"Moving to an Element {locator}")
